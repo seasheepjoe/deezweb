ee2f09e0f2c8c61b30182a7908a70a0d581908f8
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2019-01-27 20:44:13 +0100

    Added random fav feature

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,16 +4,51 @@
 			<h1 id="welcome-title">Bienvenue sur Deezweb</h1>
 			<h4 id="welcome-description">L'application qui permet facilement d'écouter de la musique.</h4>
 		</div>
+		<div v-if="$store.state.FAVORITES !== ''" class="random-favorite">
+			<Loader v-if="isLoading"/>
+			<MusicCard v-if="track !== null && !isLoading" :track="track" @onFavRemove="getRandomFav"/>
+			<button id="random-btn" @click="getRandomFav">Choisir une autre musique</button>
+		</div>
 	</div>
 </template>
 
 <script>
 import MusicCard from "@/components/MusicCard";
+import api from "@/services/ApiConfig";
+import Loader from "vue-simple-spinner";
 
 export default {
 	name: "Home",
+	beforeMount() {
+		this.getRandomFav();
+	},
 	components: {
-		MusicCard
+		MusicCard,
+		Loader
+	},
+	data() {
+		return {
+			track: null,
+			isLoading: true
+		};
+	},
+	methods: {
+		getRandomFav() {
+			this.isLoading = true;
+			let favs = this.$store.state.FAVORITES;
+			if (favs !== "") {
+				favs = favs.split(",").map(Number);
+				let randomID = favs[Math.floor(Math.random() * favs.length)];
+				api.fetchById(randomID).then(data => {
+					if (data) {
+						this.track = data;
+						this.isLoading = false;
+					}
+				});
+			} else {
+				console.warn("No favorites");
+			}
+		}
 	}
 };
 </script>
