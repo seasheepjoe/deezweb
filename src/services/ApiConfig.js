7fe2e357854603829b527af86e8cc0e4daa5f253
LouisLoiseau <louis.loiseau-billon@supinternet.fr>
2019-01-28 16:53:55 +0100

    Moved to single quote

--- src/services/ApiConfig.js
+++ src/services/ApiConfig.js
@@ -1,31 +1,17 @@
 import axios from 'axios';
 
 let api = axios.create();
-let baseUrl = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/";
 
 export default {
-	fetchAll(url) {
-		return api.get(url)
-			.then(response => {
-				if (!response.data.error) {
-					return Promise.resolve(response.data);
-				}
-			})
-			.catch(err => {
-				Promise.reject(err);
-			})
-	},
-	fetchById(id) {
-		return api.get(`${baseUrl}${id}`)
-			.then(response => {
-				if (!response.data.error) {
-					return Promise.resolve(response.data);
-				} else {
-					console.warn('Could not get track with id ' + id);
-				}
-			})
-			.catch(err => {
-				Promise.reject(err);
-			})
-	}
+  fetchAll(url) {
+    return api.get(url)
+      .then(response => {
+        if (!response.data.error) {
+          return Promise.resolve(response.data);
+        }
+      })
+      .catch(err => {
+        Promise.reject(err);
+      })
+  }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -4,45 +4,45 @@ import Vue from 'vue';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {
-		FAVORITES: localStorage.getItem('favorites') || ""
-	},
-	mutations: {
-		addFavorite(state, id) {
-			let favs = state.FAVORITES;
+  state: {
+    FAVORITES: localStorage.getItem('favorites') || ''
+  },
+  mutations: {
+    addFavorite(state, id) {
+      let favs = state.FAVORITES;
 
-			if (favs === "") {
-				favs = [];
-				favs.push(id);
-			} else {
-				favs = favs.split(',').map(Number);
-				if (!favs.includes(id)) {
-					favs.push(id);
-				}
-			}
-			localStorage.setItem('favorites', favs);
-			state.FAVORITES = localStorage.getItem('favorites');
-		},
-		removeFavorite(state, id) {
-			let favs = state.FAVORITES;
+      if (favs === '') {
+        favs = [];
+        favs.push(id);
+      } else {
+        favs = favs.split(',').map(Number);
+        if (!favs.includes(id)) {
+          favs.push(id);
+        }
+      }
+      localStorage.setItem('favorites', favs);
+      state.FAVORITES = localStorage.getItem('favorites');
+    },
+    removeFavorite(state, id) {
+      let favs = state.FAVORITES;
 
-			if (favs !== "") {
-				favs = favs.split(',').map(Number);
-				let foundIndex = favs.findIndex((el) => el === id);
-				if (foundIndex !== -1) {
-					favs.splice(foundIndex, 1);
-					localStorage.setItem('favorites', favs);
-				}
-			}
-			state.FAVORITES = localStorage.getItem('favorites');
-		}
-	},
-	actions: {
-		addFavorite(context, id) {
-			context.commit('addFavorite', id);
-		},
-		removeFavorite(context, id) {
-			context.commit('removeFavorite', id);
-		}
-	}
+      if (favs !== '') {
+        favs = favs.split(',').map(Number);
+        let foundIndex = favs.findIndex((el) => el === id);
+        if (foundIndex !== -1) {
+          favs.splice(foundIndex, 1);
+          localStorage.setItem('favorites', favs);
+        }
+      }
+      state.FAVORITES = localStorage.getItem('favorites');
+    }
+  },
+  actions: {
+    addFavorite(context, id) {
+      context.commit('addFavorite', id);
+    },
+    removeFavorite(context, id) {
+      context.commit('removeFavorite', id);
+    }
+  }
 });
